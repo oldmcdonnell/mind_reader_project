b3571398de3c784bc2e3b06ac66c95d2726c9385
oldmcdonnell <oldmcdonnell@gmail.com>
2024-04-04 11:58:54 -0400

    got buttons working

--- JS/script.js
+++ JS/script.js
@@ -1,25 +1,47 @@
 //document.body.onload = addBodyText;
 let btn = document.getElementById("dynamicButton");
 let btn2 = document.getElementById("standardNext")
+let textForPage = document.getElementById("javascriptText")
+
 
 let pageNum = 0;
 
+let reloadBtn = `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrstrinow-counterclockwise" viewBox="0 0 16 16">
+<path fill-rule="evenodd" d="M8 3a5 5 0 1 1-4.546 2.914.5.5 0 0 0-.908-.417A6 6 0 1 0 8 2z"/>
+<path d="M8 4.466V.534a.25.25 0 0 0-.41-.192L5.23 2.308a.25.25 0 0 0 0 .384l2.36 1.966A.25.25 0 0 0 8 4.466"/></svg>`
+
 function pageTurnOrReset(){
-if (pageNum = 0){
-    pageNum++
-} else {
-    pageNum = 0
+console.log('dynamic next page')
+    if (pageNum === 0){
+        pageNum++
+        btn.innerHTML = reloadBtn;
+        btn2.innerHTML = "Next"
+        btn2.className = "nextBtn"
+        addBodyText();
+
+    } else {
+        pageNum = 0
+        btn2.innerHTML = "";
+        btn2.className = "nextBtnClear"
+        addBodyText()
 }
+console.log('click')
 }
 
 function stdNextFunction(){
-    if (pageNum<4 && pageNum > 1) {
+    console.log(textArr[pageNum])
+    if (pageNum >= 1 && pageNum < 4) {
+        btn.innerHTML = reloadBtn;
+        btn2.innerHTML = "Next";
+        btn2.className = "nextBtn";
         pageNum++
-    }
+        addBodyText();
+        console.log('standard next')
+    } els
 }
 
 
-const textArr = ['page 1', 'page2', 'page3']
+const textArr = ['This is the first page, page 1', 'page2', 'page3', 'page4']
 
 function currentPage(){
     if (nextPage){
@@ -27,8 +49,8 @@ function currentPage(){
 }
 
 function addBodyText() {
-document.getElementById("javascriptText").textContent = "hello world"
-
+    textForPage.textContent = textArr[pageNum]
+    console.log("add body text", pageNum, textArr[pageNum])
 }
 
 btn.addEventListener("click", pageTurnOrReset);
