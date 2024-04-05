7beeb484b57361964631bc082a24df8097bbb19b
oldmcdonnell <oldmcdonnell@gmail.com>
2024-04-04 20:55:37 -0400

    added symbols

--- JS/script.js
+++ JS/script.js
@@ -2,6 +2,8 @@
 let btn = document.getElementById("dynamicButton");
 let btn2 = document.getElementById("standardNext")
 let textForPage = document.getElementById("javascriptText")
+let textForBottom = document.getElementById("bottomText")
+let textForSymbols = document.getElementById('symbolText')
 let goClass = "btn btn-info p-3 rounded-circle btn-lg"
 
 let pageNum = 0;
@@ -10,49 +12,89 @@ let reloadBtn = `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" 
 <path fill-rule="evenodd" d="M8 3a5 5 0 1 1-4.546 2.914.5.5 0 0 0-.908-.417A6 6 0 1 0 8 2z"/>
 <path d="M8 4.466V.534a.25.25 0 0 0-.41-.192L5.23 2.308a.25.25 0 0 0 0 .384l2.36 1.966A.25.25 0 0 0 8 4.466"/></svg>`
 
+
+
+function addSymbolText(){
+    let symbolElement = '' 
+    
+    for (let i =0; i < 100; i++){
+        symbolElement = document.createElement('div')
+        symbolElement.innerHTML = symbolList[i]
+        textForSymbols.appendChild(symbolElement)
+        console.log("add symbol text")
+    }   
+}
+    
+
 function pageTurnOrReset(){
-console.log('dynamic next page')
     if (pageNum === 0){
         pageNum++
         btn.innerHTML = reloadBtn;
-        btn2.innerHTML = "Next"
-        btn2.className = "nextBtn"
+        btn2.textContent = "Next"
+        btn2.classList.add("nextBtn")
+        btn2.classList.remove("nextBtnClear")
         addBodyText();
+        console.log("go")
 
     } else {
         pageNum = 0
-        btn.innerHTML = "GO"
+        btn.textContent = "GO"
         btn.className = goClass
-        btn2.innerHTML = "";
-        btn2.className = "nextBtnClear"
+        btn2.textContent = "";
+        btn2.classList.add("nextBtnClear")
         addBodyText()
+        console.log("reset")
 }
-console.log('click')
+console.log('click', pageNum)
 }
 
 function stdNextFunction(){
-    console.log(textArr[pageNum])
-    if (pageNum >= 1 && pageNum < 4) {
+    if (pageNum >= 1 && pageNum < 3) {
         btn.innerHTML = reloadBtn;
-        btn2.innerHTML = "Next";
-        btn2.className = "nextBtn";
+        btn2.textContent = "Next";
+ //       btn2.className = "nextBtn";
         pageNum++
-        addBodyText();
-        console.log('standard next')
-    } els
-}
-
-
-const textArr = ['This is the first page, page 1', 'page2', 'page3', 'page4']
-
-function currentPage(){
-    if (nextPage){
+        //addBodyText();
+    } else if (pageNum === 3){
+        btn2.textContent = "reveal";
+//        btn2.className = "nextBtn";
+        console.log("reveal")
+        addSymbolText()
+        pageNum++
+        //addBodyText();
+    } else if (pageNum === 4) {
+        btn2.textContent = "";
+//        btn2.className = "nextBtnClear";
+        btn2.classList.add("nextBtnClear")
+        //addBodyText();
     }
+    addBodyText();
+    console.log(pageNum)
+    console.log(textArr[pageNum])
 }
+
+
+const symbolList = ['1 - ☠', '2 - ☮', '3 - ☯', '4 - ♠', '5 - Ω', '6 - ♤', '7 - ♣', 
+'8 - ♧', '9 - ♥', '10 - ♡', '10 - ♦', '11 - ♢', '12 - ♔', '13 - ♕', '14 - ♚', '15 - ♛', 
+'16 - ⚜', '17 - ★', '18 - ♥', '19 - ✮', '20 - ✯', '21 - ☄', '22 - ☾', '23 - ☽', '24 - ☼', 
+'25 - ☀', '26 - ☁', '27 - ♥', '28 - ☃', '29 - ☻', '30 - ☺', '31 - ☹', '32 - ۞', '33 - ۩', 
+'34 - ☠', '35 - ☮', '36 - ♥', '37 - ♠', '38 - Ω', '39 - ♤', '40 - ♣', '41 - ♧', '42 - ♥', 
+'43 - ♡', '44 - ♦', '45 - ♥', '46 - ♔', '47 - ♕', '48 - ♚', '49 - ♛', '50 - ⚜', '51 - ★',
+ '52 - ♥', '53 - ✮', '54 - ♥', '55 - ☄', '56 - ☾', '57 - ☽', '58 - ☼', '59 - ☀', '60 - ☁', 
+ '61 - ⚜', '62 - ☃', '63 - ♥', '64 - ☺', '65 - ☹', '66 - ۞', '67 - ۩', '68 - ☮', '69 - ☯', 
+ '70 - ♠', '71 - Ω', '72 - ♥', '73 - ♣', '74 - ♧', '75 - ♥', '76 - ♡', '77 - ♦', '78 - ♢', '79 - ♔',
+  '80 - ♕', '81 - ♥', '82 - ♛', '83 - ⚜', '84 - ★', '85 - ♥', '86 - ✮', '87 - ✯', '88 - ☄', '89 - ☾',
+   '90 - ♥', '91 - ☼', '92 - ☀', '93 - ☁', '94 - ♥', '95 - ☃', '96 - ☻', '97 - ☺', '98 - ☹', '99 - ۞']
+const printArray = []
+const numArry = []
+
+const textArr = ['I can read your mind', 'Pick a number between 01-99', 'Add both numbers together to get a new number', 'Subtract your new number from the original number', 'Symbols', '']
+const bottomArr = ['','when you have your number click next', 'Ex: 14 is 1 + 4 =5 click next to proceed', 'Subtract your number number from your original number','find your new number', 'your symbol is  ♥']
 
 function addBodyText() {
     textForPage.textContent = textArr[pageNum]
-    console.log("add body text", pageNum, textArr[pageNum])
+    //console.log("add body text", pageNum, textArr[pageNum])
+    textForBottom.textContent = bottomArr[pageNum]
 }
 
 btn.addEventListener("click", pageTurnOrReset);
--- markdown.md
+++ markdown.md
@@ -14,3 +14,9 @@
 
 dynamic button if page is 1 go to page 2 if page is greater than 1 reset to page 1
 if page 1 image = arrow, if page is greater than one you need circle arrow image
+
+
+on 0 no next button GO button 
+on 1 go button turhs into reset button next button appears
+on 4 next button changes to reveal  but same class
+on 5 symbol presentation, next button class is clear again
